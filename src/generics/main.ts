59959d4a8ff9c25907a26ade00c64ab852f6b69e
Mohammed Murshid <t4technow@gmail.com>
2023-08-23 19:03:34 +0530

    configured ts

---
+++ src/generics/main.ts
@@ -0,0 +1,11 @@
+export class ListView {
+	constructor() {
+		console.log("ListView constructor");
+	}
+}
+
+export class CreateView {
+	constructor() {
+		console.log("CreateView constructor");
+	}
+}
